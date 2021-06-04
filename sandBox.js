1524af083c035a91320588ec1b54997bf876135d
unknown <you@example.comsajidridowan7@gmail.com>
2021-06-04 14:58:44 +0600

    add btn

--- sandBox.js
+++ sandBox.js
@@ -1,14 +1,16 @@
 const second=document.querySelector('.second');
-//
+const btn=document.querySelector('.btn');
 
 const fun=async ()=>{
     const jokes= await fetch('https://icanhazdadjoke.com/slack');
     const mainOne=await jokes.json();
     //return mainOne.attachments[0].text;
-    second.innerHTML+=`<p>${mainOne.attachments[0].text}<P>`;
+    second.innerHTML=`<p>${mainOne.attachments[0].text}<P>`;
 }
 fun();
 //fun().then(data=>console.log(data));
-
+btn.addEventListener('click',(e)=>{
+    fun()
+})
 
 
